e6c890fb4330f72250f51b99ae3e23699576a481
Jen <peyjen5@gmail.com>
2025-07-07 14:40:11 +0800

    20250707_filter

--- DjangoPrj/settings.py
+++ DjangoPrj/settings.py
@@ -59,10 +59,14 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "libraries": {
+                "markdown_tag": "libraries.filter.markdown_filter",
+            }
         },
     },
 ]
